61d187c4f8d86e2042ce89f5af8c71c25bdbb1a1
Marios Kanellopoulos <mariosknl@outlook.com>
2023-06-23 12:18:27 +0300

    fixing build errors

--- src/components/UserNameForm.tsx
+++ src/components/UserNameForm.tsx
@@ -1,10 +1,13 @@
 "use client";
 
-import { UsernameRequest, UsernameValidator } from "@/lib/validators/username";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { User } from "@prisma/client";
-import { FC } from "react";
+import { useRouter } from "next/navigation";
+import * as React from "react";
 import { useForm } from "react-hook-form";
+import * as z from "zod";
+
+import { Button } from "@/components/ui/Button";
 import {
 	Card,
 	CardContent,
@@ -12,66 +15,72 @@ import {
 	CardFooter,
 	CardHeader,
 	CardTitle,
-} from "./ui/Card";
-import { Label } from "./ui/Label";
-import { Input } from "./ui/Input";
-import { Button } from "./ui/Button";
+} from "@/components/ui/Card";
+import { Input } from "@/components/ui/Input";
+import { Label } from "@/components/ui/Label";
+import { toast } from "@/hooks/use-toast";
+import { cn } from "@/lib/utils";
+import { UsernameValidator } from "@/lib/validators/username";
 import { useMutation } from "@tanstack/react-query";
 import axios, { AxiosError } from "axios";
-import { toast } from "@/hooks/use-toast";
-import { useRouter } from "next/navigation";
 
-interface UserNameFormProps {
+interface UserNameFormProps extends React.HTMLAttributes<HTMLFormElement> {
 	user: Pick<User, "id" | "username">;
 }
 
-const UserNameForm: FC<UserNameFormProps> = ({ user }) => {
+type FormData = z.infer<typeof UsernameValidator>;
+
+export function UserNameForm({ user, className, ...props }: UserNameFormProps) {
+	const router = useRouter();
 	const {
 		handleSubmit,
 		register,
 		formState: { errors },
-	} = useForm<UsernameRequest>({
+	} = useForm<FormData>({
 		resolver: zodResolver(UsernameValidator),
 		defaultValues: {
 			name: user?.username || "",
 		},
 	});
-	const router = useRouter();
 
 	const { mutate: updateUsername, isLoading } = useMutation({
-		mutationFn: async ({ name }: UsernameRequest) => {
-			const payload: UsernameRequest = { name };
+		mutationFn: async ({ name }: FormData) => {
+			const payload: FormData = { name };
 
-			const { data } = axios.patch(`/api/username`, payload);
+			const { data } = await axios.patch(`/api/username/`, payload);
 			return data;
 		},
 		onError: (err) => {
 			if (err instanceof AxiosError) {
 				if (err.response?.status === 409) {
 					return toast({
-						title: "Username already taken",
-						description: "Please choose a different username",
+						title: "Username already taken.",
+						description: "Please choose another username.",
 						variant: "destructive",
 					});
 				}
 			}
 
-			toast({
-				title: "Something went wrong",
-				description: "Please try again later",
+			return toast({
+				title: "Something went wrong.",
+				description: "Your username was not updated. Please try again.",
 				variant: "destructive",
 			});
 		},
 		onSuccess: () => {
 			toast({
-				description: "Your username has been updated!",
+				description: "Your username has been updated.",
 			});
 			router.refresh();
 		},
 	});
 
 	return (
-		<form onSubmit={handleSubmit((e) => updateUsername(e))}>
+		<form
+			className={cn(className)}
+			onSubmit={handleSubmit((e) => updateUsername(e))}
+			{...props}
+		>
 			<Card>
 				<CardHeader>
 					<CardTitle>Your username</CardTitle>
@@ -79,13 +88,11 @@ const UserNameForm: FC<UserNameFormProps> = ({ user }) => {
 						Please enter a display name you are comfortable with.
 					</CardDescription>
 				</CardHeader>
-
 				<CardContent>
-					<div className="relative gap-1">
+					<div className="relative grid gap-1">
 						<div className="absolute top-0 left-0 w-8 h-10 grid place-items-center">
 							<span className="text-sm text-zinc-400">u/</span>
 						</div>
-
 						<Label className="sr-only" htmlFor="name">
 							Name
 						</Label>
@@ -100,13 +107,10 @@ const UserNameForm: FC<UserNameFormProps> = ({ user }) => {
 						)}
 					</div>
 				</CardContent>
-
 				<CardFooter>
 					<Button isLoading={isLoading}>Change name</Button>
 				</CardFooter>
 			</Card>
 		</form>
 	);
-};
-
-export default UserNameForm;
+}
