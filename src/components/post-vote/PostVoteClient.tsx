05db5116bb7e3abaa6008ff2fbc177c20b686075
Marios Kanellopoulos <mariosknl@outlook.com>
2023-06-23 07:09:32 +0300

    advanced caching: optimizing post performance

--- src/components/post-vote/PostVoteClient.tsx
+++ src/components/post-vote/PostVoteClient.tsx
@@ -1,15 +1,16 @@
 "use client";
 
 import { useCustomToast } from "@/hooks/use-custom-toast";
+import { PostVoteRequest } from "@/lib/validators/vote";
 import { usePrevious } from "@mantine/hooks";
 import { VoteType } from "@prisma/client";
-import { FC, useEffect, useState } from "react";
+import { useMutation } from "@tanstack/react-query";
+import axios, { AxiosError } from "axios";
+import { useEffect, useState } from "react";
+import { toast } from "../../hooks/use-toast";
 import { Button } from "../ui/Button";
 import { ArrowBigDown, ArrowBigUp } from "lucide-react";
 import { cn } from "@/lib/utils";
-import { useMutation } from "@tanstack/react-query";
-import { PostVoteRequest } from "@/lib/validators/vote";
-import axios from "axios";
 
 interface PostVoteClientProps {
 	postId: string;
@@ -17,34 +18,74 @@ interface PostVoteClientProps {
 	initialVote?: VoteType | null;
 }
 
-const PostVoteClient: FC<PostVoteClientProps> = ({
+const PostVoteClient = ({
 	postId,
 	initialVotesAmt,
 	initialVote,
-}) => {
+}: PostVoteClientProps) => {
 	const { loginToast } = useCustomToast();
 	const [votesAmt, setVotesAmt] = useState<number>(initialVotesAmt);
 	const [currentVote, setCurrentVote] = useState(initialVote);
 	const prevVote = usePrevious(currentVote);
 
+	// ensure sync with server
 	useEffect(() => {
 		setCurrentVote(initialVote);
 	}, [initialVote]);
 
-	const {} = useMutation({
-		mutationFn: async (voteType: VoteType) => {
+	const { mutate: vote } = useMutation({
+		mutationFn: async (type: VoteType) => {
 			const payload: PostVoteRequest = {
-				postId,
-				voteType,
+				voteType: type,
+				postId: postId,
 			};
 
-			await axios.patch("api/subreddit/post/vote", payload);
+			await axios.patch("/api/subreddit/post/vote", payload);
+		},
+		onError: (err, voteType) => {
+			if (voteType === "UP") setVotesAmt((prev) => prev - 1);
+			else setVotesAmt((prev) => prev + 1);
+
+			// reset current vote
+			setCurrentVote(prevVote);
+
+			if (err instanceof AxiosError) {
+				if (err.response?.status === 401) {
+					return loginToast();
+				}
+			}
+
+			return toast({
+				title: "Something went wrong.",
+				description: "Your vote was not registered. Please try again.",
+				variant: "destructive",
+			});
+		},
+		onMutate: (type: VoteType) => {
+			if (currentVote === type) {
+				// User is voting the same way again, so remove their vote
+				setCurrentVote(undefined);
+				if (type === "UP") setVotesAmt((prev) => prev - 1);
+				else if (type === "DOWN") setVotesAmt((prev) => prev + 1);
+			} else {
+				// User is voting in the opposite direction, so subtract 2
+				setCurrentVote(type);
+				if (type === "UP") setVotesAmt((prev) => prev + (currentVote ? 2 : 1));
+				else if (type === "DOWN")
+					setVotesAmt((prev) => prev - (currentVote ? 2 : 1));
+			}
 		},
 	});
 
 	return (
-		<div className="flex sm:flex-col gap-4 sm:gap-0 pr-6 sm:w-20 pb-4 sm:pb-0">
-			<Button size="sm" variant="ghost" aria-label="upvote">
+		<div className="flex flex-col gap-4 sm:gap-0 pr-6 sm:w-20 pb-4 sm:pb-0">
+			{/* upvote */}
+			<Button
+				onClick={() => vote("UP")}
+				size="sm"
+				variant="ghost"
+				aria-label="upvote"
+			>
 				<ArrowBigUp
 					className={cn("h-5 w-5 text-zinc-700", {
 						"text-emerald-500 fill-emerald-500": currentVote === "UP",
@@ -52,11 +93,21 @@ const PostVoteClient: FC<PostVoteClientProps> = ({
 				/>
 			</Button>
 
+			{/* score */}
 			<p className="text-center py-2 font-medium text-sm text-zinc-900">
 				{votesAmt}
 			</p>
 
-			<Button size="sm" variant="ghost" aria-label="downvote">
+			{/* downvote */}
+			<Button
+				onClick={() => vote("DOWN")}
+				size="sm"
+				className={cn({
+					"text-emerald-500": currentVote === "DOWN",
+				})}
+				variant="ghost"
+				aria-label="upvote"
+			>
 				<ArrowBigDown
 					className={cn("h-5 w-5 text-zinc-700", {
 						"text-red-500 fill-red-500": currentVote === "DOWN",
