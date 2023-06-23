161535cb1da051203b55cff5372cdc5e8fc7038d
Marios Kanellopoulos <mariosknl@outlook.com>
2023-06-23 09:23:26 +0300

    setting comments 1/2

--- src/app/r/[slug]/post/[postId]/page.tsx
+++ src/app/r/[slug]/post/[postId]/page.tsx
@@ -1,7 +1,10 @@
+import CommentsSection from "@/components/CommentsSection";
+import EditorOutput from "@/components/EditorOutput";
 import PostVoteServer from "@/components/post-vote/PostVoteServer";
 import { buttonVariants } from "@/components/ui/Button";
 import { db } from "@/lib/db";
 import { redis } from "@/lib/redis";
+import { formatTimeToNow } from "@/lib/utils";
 import { CachedPost } from "@/types/redis";
 import { Post, User, Vote } from "@prisma/client";
 import { ArrowBigDown, ArrowBigUp, Loader2 } from "lucide-react";
@@ -56,6 +59,27 @@ const page = async ({ params }: PageProps) => {
 						}}
 					/>
 				</Suspense>
+
+				<div className="sm:w-0 w-full flex-1 bg-white p-4 rounded-sm">
+					<p className="max-h-40 mt-1 truncate text-xs text-gray-500">
+						Posted by u/{post?.author.username ?? cachedPost.authorUsername}{" "}
+						{formatTimeToNow(new Date(post?.createdAt ?? cachedPost.createdAt))}
+					</p>
+					<h1 className="text-xl font-semibold py-2 leading-6 text-gray-900">
+						{post?.title ?? cachedPost.title}
+					</h1>
+
+					<EditorOutput content={post?.content ?? cachedPost?.content} />
+
+					<Suspense
+						fallback={
+							<Loader2 className="h-5 w-5 animate-spin text-zinc-500" />
+						}
+					>
+						{/* @ts-expect-error server component */}
+						<CommentsSection postId={post?.id ?? cachedPost.id} />
+					</Suspense>
+				</div>
 			</div>
 		</div>
 	);
